d6bfd5268ae76ee4e6f227effd9a4e55807dcfa0
symplyijay <ijeonyi@gmail.com>
2022-06-09 08:43:41 +0100

    news-article website

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react';
 import Article from './Article'
 import SearchForm from './SearchForm';
+import UseFetch from './UseFetch';
 
 const Home = () => {
-    const [articles, setArticles] = useState([])
-    // const [search, setSearch] = useState('');
-    const [term, setTerm]=useState('everything')
-    const [isLoading, setIsLoading] = useState(true)
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${term}&api-key=${process.env.REACT_APP_ARTICLES_API_KEY}`)
-                .then((res) => {
-                    if (!res.ok) {
-                        throw Error("can't connect");
-                    }
-                    return res.json();
-                })
-                .then((data) => {
-                    setArticles(data.response.docs);
-                    setError(null);
-                    setIsLoading(false);
-                })
-                .catch((err) => {
-                    setError(err.message);
-                    setIsLoading(false);
-                });
-
-            
-        }, 300);
-        
-    },[term])
-    return (
+    
+    const [term, setTerm] = React.useState('everything')
+    const {data: articles,isLoading} = UseFetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=everything&api-key=LHIAYOdenbfG9o5gmlRlNp2SGDLZ2Egj`);
+    
+        return (
 			<>
 				<div className='showcase mb-8'>
 					<div className='overlay'>
@@ -44,9 +20,8 @@ const Home = () => {
 					</div>
             </div>
             <div className='container mx-auto'>
-					{error && <div className="text-center text-3xl mt-20 font-bold">{error}</div>}
-					{isLoading && <div className='text-center text-3xl mt-20 font-bold'>Loading...</div>}
-					{articles && <Article articles={articles} />}
+				{isLoading && <div className='text-center text-3xl mt-20 font-bold'>Loading...</div>}
+				{articles && <Article articles={articles} />}
 				</div>
 			</>
 		);
