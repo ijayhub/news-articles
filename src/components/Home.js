20f966eef57855a97cdcfc17e77898e12f8ea23d
symplyijay <ijeonyi@gmail.com>
2022-06-09 09:11:23 +0100

    news-article website

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,7 @@ import UseFetch from './UseFetch';
 const Home = () => {
     
     const [term, setTerm] = React.useState('everything')
-    const { data: articles, isLoading } = UseFetch(
-			`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=everything&api-key=${process.env.REACT_APP_ARTICLES_API_KEY}`
+    const { data: articles, isLoading } = UseFetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${term}&api-key=${process.env.REACT_APP_ARTICLES_API_KEY}`
 		);
     
         return (
